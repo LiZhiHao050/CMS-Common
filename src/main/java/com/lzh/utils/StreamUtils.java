36985f239c6081597677e939965485df7f005eb9
LiZhiHao050 <1224120673@qq.com>
2019-10-12 09:40:54 +0800

    第一次提交:流处理工具类

---
+++ src/main/java/com/lzh/utils/StreamUtils.java
@@ -0,0 +1,29 @@
+package com.lzh.utils;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author LZH
+ * @Date 2019年10月12日
+ * 流处理工具类
+ */
+
+public class StreamUtils {
+
+	/**
+	 *  关闭所有的流
+	 * @param n
+	 * @param streams
+	 * @throws IOException
+	 */                           // 可变参数
+	public static void closeStream(Closeable ... streams) throws IOException {
+		
+		for (int i = 0; i < streams.length; i++) {
+			streams[i].close();
+		}
+		
+	}
+	
+}
+
