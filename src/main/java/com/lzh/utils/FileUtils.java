253be86ca3e1f44d4006120d5c4340bf1dc8f4fe
LiZhiHao050 <1224120673@qq.com>
2019-10-13 18:44:28 +0800

    第三次修改:新增读取文件内容方法

--- src/main/java/com/lzh/utils/FileUtils.java
+++ src/main/java/com/lzh/utils/FileUtils.java
@@ -2,9 +2,15 @@ package com.lzh.utils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * @author LZH
@@ -91,6 +97,50 @@ public class FileUtils {
 		}
 		
 	}
+	
+	
+	/**
+	 * 按行读取文件
+	 * @return
+	 * @throws IOException 
+	 */
+	public static String readFileByLine(String fileName) throws IOException {
+		
+		StringBuilder sb = new StringBuilder();
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader reader = new InputStreamReader(fis);
+		BufferedReader  bufferedReader = new BufferedReader(reader);
+		String str = null;
+		while( (str=bufferedReader.readLine())!=null ) {
+			sb.append(str).append("\r\n");// 追加
+		}
+		//关闭流
+		StreamUtils.closeStream(fis);
+		return sb.toString();
+		
+	} 
+	
+	/**
+	 * 读取文件
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 */
+	public static List<String> readFile(String fileName) throws IOException {
+		List<String> strList = new ArrayList<>();
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader reader = new InputStreamReader(fis);
+		BufferedReader  bufferedReader = new BufferedReader(reader);
+		String str = null;
+		while( (str=bufferedReader.readLine())!=null ) {
+			strList.add(str);
+		}
+		//关闭流
+		StreamUtils.closeStream(fis);
+		return strList;
+	} 
 
 }
 
