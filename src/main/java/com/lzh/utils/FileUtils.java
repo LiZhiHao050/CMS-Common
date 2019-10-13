f8b40cadb2aa616588f99715d3b89fb176815900
LiZhiHao050 <1224120673@qq.com>
2019-10-13 17:58:12 +0800

    第二次修改:新增删除文件方法

--- src/main/java/com/lzh/utils/FileUtils.java
+++ src/main/java/com/lzh/utils/FileUtils.java
@@ -9,13 +9,13 @@ import java.io.FileOutputStream;
 /**
  * @author LZH
  * @Date 2019年10月12日
- * 文件工具类
+ *	文件工具类
  */
 
 public class FileUtils {
 	
 	/**
-	 * 
+	 * 复制文件
 	 * @param src 源文件目录
 	 * @param dst 目标文件目录
 	 * @throws Exception 
@@ -60,6 +60,35 @@ public class FileUtils {
 		
 		//调用流工具类 去关闭流
 		StreamUtils.closeStream(fis,fos);*/
+		
+	}
+	
+	
+	/**
+	 * 删除文件
+	 * @param path 要删除的文件路径
+	 */
+	public static void del(String path) {
+		
+		File file = new File(path);
+		
+		if (!file.exists()) {
+			System.out.println("该文件不存在");
+			return;
+		}
+		
+		if (file.isFile()) {      // 如果是文件
+			System.out.println("删除文件:" + path);
+			file.delete();
+		} else {                  // 否则是路径
+			String[] list = file.list();
+			for (String str : list) {
+				String subFileName = path + "\\" + str;
+				del(subFileName);
+			}
+			System.out.println("删除目录  " + path);
+			file.delete();
+		}
 		
 	}
 
